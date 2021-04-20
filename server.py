150d188a9b865714fa439d166bc1dcec465e9d09
kristiyansakalyan <57534207+kristiyansakalyan@users.noreply.github.com>
2021-04-20 10:07:34 +0300

    Db connection and initial route;

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import db
 # Create the application instance
 app = Flask(__name__)
 
+
 # Create a URL route in our application for "/"
-@app.route('/')
-def home():
+@app.route('/popularity/<symbol>')
+def home(symbol):
     """
-    This function just responds to the browser ULR
-    localhost:5000/
+    This functions takes the symbol as parameter, 
+    finds the stock datapoints in MongoDB and return them.
 
     :return:
     """
-    return "Hello"
+    result = db.findStockBySymbol(symbol)
+    return jsonify(result)
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
+    
